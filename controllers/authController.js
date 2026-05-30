6a0706cf7cf5bc045928e0d5e25af579e8f55bf5
santoshmode07 <santoshmode7@gmail.com>
2026-05-30 10:45:34 +0530

    Added current user profile API

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,10 +87,28 @@ const loginUser = async (req, res) => {
 };
 
 const getProfile = async (req, res) => {
-  res.status(200).json({
-    success: true,
-    user: req.user,
-  });
+  try {
+
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
 };
 
 module.exports = {
